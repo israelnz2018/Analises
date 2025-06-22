407d050f0d0f7d95ff69fdfaf92e067cc7a9f68c
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-06-23 09:54:04 +1200

    Update leitura.py

--- n8n/leitura.py
+++ n8n/leitura.py
@@ -6,10 +6,11 @@ from fastapi.responses import JSONResponse
 
 async def ler_arquivo(arquivo):
     if arquivo and arquivo.filename.endswith(".xlsx"):
-        await arquivo.seek(0)
+        await arquivo.seek(0)  # Garante que a leitura começa do início
         file_bytes = await arquivo.read()
         df = pd.read_excel(io.BytesIO(file_bytes), engine="openpyxl")
         df.columns = df.columns.map(str).map(str.strip)
+        print("🚀 Nome do arquivo recebido:", arquivo.filename)
         print("🚀 Colunas realmente lidas no arquivo:", df.columns.tolist())
         return df
     else:
