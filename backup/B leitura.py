efe1f55dd50c40f7e556e3daaee795b33e460b32
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-07-01 09:16:05 +1200

    Update and rename leitura.py to B leitura.py

--- backup/B leitura.py
+++ backup/B leitura.py
@@ -1,15 +1,15 @@
-
 import pandas as pd
 import io
 import os
 
 from fastapi.responses import JSONResponse
 
-async def ler_arquivo(arquivo):
+async def ler_arquivo(arquivo, aba=None):
     if arquivo and arquivo.filename.endswith(".xlsx"):
+        await arquivo.seek(0)
         file_bytes = await arquivo.read()
-        df = pd.read_excel(io.BytesIO(file_bytes), engine="openpyxl")
-        df.columns = df.columns.str.strip()
+        df = pd.read_excel(io.BytesIO(file_bytes), engine="openpyxl", sheet_name=aba)
+        df.columns = df.columns.map(str).map(str.strip)
         return df
     else:
         raise ValueError("Envie um arquivo Excel (.xlsx) válido.")
