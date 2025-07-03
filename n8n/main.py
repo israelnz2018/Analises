c8852e22a825027eaddabcbf0a73660b0c27d7a0
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-07-03 16:41:54 +1200

    Update main.py

--- n8n/main.py
+++ n8n/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, Form, Request, Response
+from fastapi import FastAPI, File, UploadFile, Form, Request, Response 
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware  
 import traceback 
